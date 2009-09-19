fb5517222ce2113ba68ce91d1e7ed4eb1673b888
Alex Clark <aclark@aclark.net>
2009-09-19 03:35:39 +0000

    Move trunk of Products.project here to take advantage of z3c.autoinclude, among other reasons.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6'
+
+setup(name='collective.project',
+      version=version,
+      description="Formerly Products.project, this package aims for "simple" project management with 3 new (Dexterity) content types added to Plone: Project, Iteration, and Task.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='"project management"',
+      author='Alex Clark',
+      author_email='aclark@aclark.net',
+      url='https://svn.plone.org/svn/collective/collective.project/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
