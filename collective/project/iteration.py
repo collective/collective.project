26589d61d3dae064140d2f9a5dc45b5871b9ee20
Alex Clark <aclark@aclark.net>
2009-09-24 22:51:57 +0000

    Set default title for iteration!

--- collective/project/iteration.py
+++ collective/project/iteration.py
@@ -1,30 +1,19 @@
-"""This interface is used by the example.schemapage type. The interface
-below is "real", and you can register views and adapters for it. It
-will be populated with schema fields read from page.xml in this 
-directory when the package is grokked.
-
-It is possible to amend/customise the interface with new fields in addition
-to those found in the model file, although without a class we normally can't
-promise new methods.
-
-We also register a custom view, called @@view. The template for this is found
-in page_templates/view.pt, because this module is called "page.py" and the
-view class is called "View". We specify that it is a view for any IPage
-(grok.context) and requires the View permission (grok.require).
-"""
-
 from five import grok
 from zope import schema
 from plone.directives import form
 from collective.project import projectMessageFactory as _
 import datetime
+import calendar
 
 class IIteration(form.Schema):
-#    form.model("models/project.xml")
-    
-    # It is possible to add additional fields and methods can be added here
-    # if necessary. However, without a custom class, we usually can't
-    # promise new methods.
+    title = schema.TextLine(
+            title=_(u"Title"),
+        )
+
+    description = schema.Text(
+            title=_(u"Description"),
+            required=False,
+        )
 
     start = schema.Datetime(
             title=_(u"Start date"),
@@ -34,12 +23,6 @@ class IIteration(form.Schema):
     stop = schema.Datetime(
             title=_(u"End date"),
             required=False,
-        )
-
-    hours = schema.Float(
-            title=_(u"Hours"),
-            required=False,
-            default=0.0,
         )
 
 class View(grok.View):
@@ -130,4 +113,9 @@ def startDate(data):
 
 @form.default_value(field=IIteration['stop'])
 def stopDate(data):
-    return datetime.datetime.today() + datetime.timedelta(30)
+    # stop in one month
+    return datetime.datetime.today() + datetime.timedelta(days=calendar.mdays[datetime.date.today().month])
+
+@form.default_value(field=IIteration['title'])
+def iterTitle(data):
+    return datetime.datetime.today().strftime('%B %Y')
--- collective/project/project.py
+++ collective/project/project.py
@@ -1,18 +1,3 @@
-"""This interface is used by the example.schemapage type. The interface
-below is "real", and you can register views and adapters for it. It
-will be populated with schema fields read from page.xml in this 
-directory when the package is grokked.
-
-It is possible to amend/customise the interface with new fields in addition
-to those found in the model file, although without a class we normally can't
-promise new methods.
-
-We also register a custom view, called @@view. The template for this is found
-in page_templates/view.pt, because this module is called "page.py" and the
-view class is called "View". We specify that it is a view for any IPage
-(grok.context) and requires the View permission (grok.require).
-"""
-
 from five import grok
 from zope import schema
 from plone.directives import form
@@ -20,12 +5,6 @@ from collective.project import projectMessageFactory as _
 import datetime
 
 class IProject(form.Schema):
-#    form.model("models/project.xml")
-    
-    # It is possible to add additional fields and methods can be added here
-    # if necessary. However, without a custom class, we usually can't
-    # promise new methods.
-
     start = schema.Datetime(
             title=_(u"Start date"),
             required=False,
@@ -148,4 +127,5 @@ def startDate(data):
 
 @form.default_value(field=IProject['stop'])
 def stopDate(data):
-    return datetime.datetime.today() + datetime.timedelta(365)
+    # stop in one year
+    return datetime.datetime.today() + datetime.timedelta(days=365)
