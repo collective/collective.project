4e36c11f63ed7c575cdecc5172195aef8eee45fa
Alex Clark <aclark@aclark.net>
2009-09-25 00:53:48 +0000

    Add template for task, based on iteration template. Shows total hours as str(stop - start).

--- collective/project/task.py
+++ collective/project/task.py
@@ -31,6 +31,87 @@ class ITask(form.Schema):
             required=False,
         )
 
+class View(grok.View):
+    grok.context(ITask)
+    grok.require('zope2.View')
+
+    def getIterTitle(self,project):
+        client_breadcrumbs = self.client_breadcrumbs(project)
+        try:
+            return self.context.portal_properties.project_properties.iteration + ' &rarr; ' + client_breadcrumbs
+        except:
+            return 'Active Projects &rarr; ' + client_breadcrumbs
+
+    def client_breadcrumbs(self,project):
+        results = []
+        path = list(project.getPhysicalPath())[2:]
+        for i in range(len(path)):
+            results.append(self.context.restrictedTraverse('/'.join(path)).Title())
+            path.pop()
+        results.reverse()
+        return ' &rarr; '.join(results)
+
+    def total_hours(self):
+        try:
+            hours = 0.0
+            tasks = self.context.aq_inner.objectValues()
+            for task in tasks:
+                hours += task.hours
+        except:
+            hours = 0.0
+
+        return self.format_float(hours)
+
+    def format_float(self,f):
+        try:
+            f = '%.2f' % f
+            return f
+        except:
+            return None
+
+    def getRate(self):
+        return self.format_float(self.context.aq_inner.rate)
+
+    def total_income(self):
+        try:
+            return self.format_float(self.calculate_billable() * self.context.aq_inner.rate)
+        except:
+            return self.format_float(self.calculate_billable() * 0.0)
+
+    def calculate_billable(self):
+        try:
+            hours = 0.0
+            tasks = self.context.aq_inner.objectValues()
+            for task in tasks:
+                if task.billable:
+                    hours += task.hours
+        except:
+            hours = 0.0
+
+        return hours
+
+    def getOddEven(self,counter):
+        if counter % 2 == 0:
+            return 'even'
+        else:
+            return 'odd'
+
+    def getStartDate(self):
+        return self.format_date(self.context.start)
+
+    def getStopDate(self):
+        return self.format_date(self.context.stop)
+
+    def format_date(self,d):
+        try:
+            d = d.strftime('%Y-%m-%d')
+            return d
+        except:
+            return None
+
+    def getHours(self):
+        return str(self.context.stop - self.context.start)
+
 @form.default_value(field=ITask['start'])
 def startDate(data):
     return datetime.datetime.today()
