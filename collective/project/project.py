b3c5c9a2a3920e03087482ae57ec3e14d6304ce4
Alex Clark <aclark@aclark.net>
2009-09-25 19:02:13 +0000

    Template fixes and bug fix for zero rate.

--- collective/project/project.py
+++ collective/project/project.py
@@ -68,7 +68,10 @@ class View(grok.View):
         seconds = days * 86400
         hours = float((self.total_hours_billable(iter).seconds + seconds)/3600)
         rate = self.getRate()
-        return self.format_float(hours * rate)
+        try:
+            return self.format_float(hours * rate)
+        except:
+            return self.format_float(hours * 0.0)
 
     def project_title(self):
         project = self.context.Title()
