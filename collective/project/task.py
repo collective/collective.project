c2c527bbe1322356d0e07407259f8e57fa8b3615
Alex Clark <aclark@aclark.net>
2009-09-25 00:00:19 +0000

    Task now features incremental integer ids thanks to some Joel Burton Fu: http://tinyurl.com/yd73me4 (still need a better implementation e.g. one that can reset counter according to number of objects in the container)

--- collective/project/task.py
+++ collective/project/task.py
@@ -1,10 +1,25 @@
 from five import grok
 from zope import schema
-from plone.directives import form
+from plone.directives import form, dexterity
 from collective.project import projectMessageFactory as _
 import datetime
+from BTrees.Length import Length
 
 class ITask(form.Schema):
+    id = schema.TextLine(
+            title=_(u"Id"),
+        )
+
+    title = schema.TextLine(
+            title=_(u"Title"),
+        )
+
+    summary = schema.Text(
+            title=_(u"Summary"),
+            description=_(u"A short summary of the content."),
+            required=False,
+        )
+
     start = schema.Datetime(
             title=_(u"Start time"),
             required=False,
@@ -23,3 +38,13 @@ def startDate(data):
 def stopDate(data):
     # stop in one hour
     return datetime.datetime.today() + datetime.timedelta(hours=1)
+
+@form.default_value(field=ITask['id'])
+def getNextCounter(self): # Some (slightly modified) Joel Burton Fu, from SimpleCollector.
+        """Get next ID. Lazily creates counter if neccessary.
+        """
+        self = self.context
+        if not hasattr(self, '_counter'):
+            self._counter = Length(0)
+        self._counter.change(+1)
+        return self._counter()
