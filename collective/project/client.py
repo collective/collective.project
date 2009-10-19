b11354ef08ae7dabb32d75cd8a1de2f3aeb9e480
Alex Clark <aclark@aclark.net>
2009-10-19 19:24:00 +0000

    OOPS, client behaves like dublin core atm

--- collective/project/client.py
+++ collective/project/client.py
@@ -5,17 +5,6 @@ from collective.project import projectMessageFactory as _
 import datetime
 
 class IClient(form.Schema):
-    title = schema.TextLine(
-            title=_(u"Title"),
-            default=_(u"Client"),
-        )
-
-    summary = schema.Text(
-            title=_(u"Summary"),
-            description=_(u"A short summary of the content."),
-            required=False,
-        )
-
     email = schema.TextLine(
             title=_(u"Email address"),
             required=False,
