1d2c068030a2ac2f9873cdcb060817891bfe1e89
Alex Clark <aclark@aclark.net>
2009-12-01 00:33:27 +0000

    Remove twitter theme and dexterity examples

--- collective/project/client.py
+++ collective/project/client.py
@@ -3,9 +3,33 @@ from zope import schema
 from plone.directives import form
 from collective.project import projectMessageFactory as _
 import datetime
+import calendar
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+from Products.CMFCore.utils import getToolByName
 
 class IClient(form.Schema):
+
+    name = schema.Choice(
+            title=_(u"Name"),
+            source=projectTypes,
+            required=True,
+        )
+
+    summary = schema.Text(
+            title=_(u"Summary"),
+            description=_(u"A short summary of the content."),
+            required=False,
+        )
+
     email = schema.TextLine(
             title=_(u"Email address"),
             required=False,
         )
+
+class View(grok.View):
+    grok.context(IClient)
+    grok.require('zope2.View')
+
+    def disable_border(self):
+        return self.context.portal_properties.project_properties.disable_border
