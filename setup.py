fd431605d44044df0f41cafd9b956f5d506d5756
Alex Clark <aclark@aclark.net>
2011-08-16 19:43:25 -0400

    WS

--- setup.py
+++ setup.py
@@ -3,35 +3,36 @@ import os
 
 version = '0.9.5'
 
-setup(name='collective.project',
-      version=version,
-      description="""Dexterity demo: this package aims for "simple" project management by adding four new Dexterity content types: Client, Project, Iteration, and Task.""",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "INSTALL.txt")).read() +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='"project management"',
-      author='Alex Clark',
-      author_email='aclark@aclark.net',
-      url='https://github.com/collective/collective.project',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.dexterity',
-          # -*- Extra requirements: -*-
+setup(
+    name='collective.project',
+    version=version,
+    description="""Dexterity demo: this package aims for "simple" project management by adding four new Dexterity content types: Client, Project, Iteration, and Task.""",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "INSTALL.txt")).read() +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      "Framework :: Plone",
+      "Programming Language :: Python",
+      "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    keywords='"project management"',
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    url='https://github.com/collective/collective.project',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.dexterity',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
