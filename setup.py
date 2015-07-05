7e740aff58b1f48fd6e898002e4eea3e52c507b5
Alex Clark <aclark@aclark.net>
2015-07-05 07:55:57 -0400

    Clean up and add Plone 5 support
    - Update setup.py

--- setup.py
+++ setup.py
@@ -1,58 +1,43 @@
 from setuptools import find_packages
 from setuptools import setup
-import os
 
-VERSION = '1.0.1'
+
+VERSION = '2.0.0.dev0'
 
 
 setup(
     author='Alex Clark',
     author_email='aclark@aclark.net',
-    description='Demo app: project management with dexterity content types.',
+    description='Project Management With Dexterity Content Types',
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone',
     },
-    extras_require = {
-        'test': [
-            'plone.app.testing',
-            'unittest2'
-            ],
-        'grok': [
-            'five.grok',
-            'plone.directives.dexterity',
-            'plone.directives.form >=1.1dev',
-            ],
-        'relations': [
-            'plone.app.relationfield',
-            'plone.app.intid',
-            'z3c.relationfield',
-            ]
-    },
+    extras_require={},
     classifiers=[
-      "Framework :: Plone",
-      "Framework :: Plone :: 4.0",
-      "Framework :: Plone :: 4.1",
-      "Framework :: Plone :: 4.2",
-      "Framework :: Plone :: 4.3",
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
     include_package_data=True,
     install_requires=[
         'setuptools',
-        'plone.app.dexterity [grok]',
+        'plone.app.dexterity',
     ],
-    keywords='add-on plone dexterity example package',
+    keywords='dexterity plone project management',
     license='ZPL',
     long_description=(
         open("README.rst").read() +
-        open(os.path.join("docs", "HISTORY.txt")).read()
+        open("CHANGES.rst").read()
     ),
     namespace_packages=[
         'collective'
     ],
     packages=find_packages(),
-    url='http://collective.github.com/collective.project/',
+    url='https://github.com/collective/collective.project',
     name='collective.project',
     version=VERSION,
     zip_safe=False,
